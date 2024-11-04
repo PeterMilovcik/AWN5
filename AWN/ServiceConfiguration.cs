37ea80916ee0556563d40e2fdc95968effae499a
Peter Milovcik <peter.milovcik@gmail.com>
2024-11-04 11:52:16 +0100

    Add Playwright service configuration and launch browser

--- AWN/ServiceConfiguration.cs
+++ AWN/ServiceConfiguration.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Playwright;
 
 namespace AWN
 {
@@ -9,6 +10,14 @@ namespace AWN
             services.AddSingleton<CommandInvoker>();
             services.AddSingleton<ICommand, ExitCommand>();
             services.AddSingleton<ICommand, NavigateCommand>(); // Register NavigateCommand
+
+            services.AddSingleton(serviceProvider =>
+            {
+                var playwright = Playwright.CreateAsync().GetAwaiter().GetResult();
+                var browser = playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false }).GetAwaiter().GetResult();
+                var page = browser.NewPageAsync().GetAwaiter().GetResult();
+                return page;
+            });
         }
     }
 }
